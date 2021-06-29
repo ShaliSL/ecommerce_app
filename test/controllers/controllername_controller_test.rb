4884e4163bbd2891cbd4e193165f41107e53ed9c
shalisl <42828750+ShaliSL@users.noreply.github.com>
2021-06-29 16:59:25 +0530

    Add files via upload

---
+++ test/controllers/controllername_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class ControllernameControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get controllername_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get controllername_create_url
+    assert_response :success
+  end
+end
