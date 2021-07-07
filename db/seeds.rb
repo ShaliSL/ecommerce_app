17973171a6a686d3bd97899cae9c0ac8d72111ac
shalisl <42828750+ShaliSL@users.noreply.github.com>
2021-07-07 17:54:27 +0530

    Add files via upload

---
+++ db/seeds.rb
@@ -0,0 +1,44 @@
+puts "Seeding Data ..."
+
+# Helper functions
+def open_asset(file_name)
+  File.open(Rails.root.join('db', 'seed_assets', file_name))
+end
+
+## CATEGORIES
+
+puts "Finding or Creating Categories ..."
+
+cat1 = Category.find_or_create_by! name: 'Apparel'
+cat2 = Category.find_or_create_by! name: 'Electronics'
+cat3 = Category.find_or_create_by! name: 'Furniture'
+
+## PRODUCTS
+
+puts "Re-creating Products ..."
+
+Product.destroy_all
+
+cat1.products.create!({
+  name:  'Men\'s Classy shirt',
+  description: Faker::Hipster.paragraph(4),
+  image: open_asset('cotton kurti.png'),
+  quantity: 10,
+  price: 64.99
+})
+
+cat1.products.create!({
+  name:  'Women\'s Zebra pants',
+  description: Faker::Hipster.paragraph(4),
+  image: open_asset('blue t-shirt.png'),
+  quantity: 18,
+  price: 124.99
+})
+
+cat1.products.create!({
+  name:  'Hipster Hat',
+  description: Faker::Hipster.paragraph(4),
+  image: open_asset('party wear.png'),
+  quantity: 0,
+  price: 34.49
+})
