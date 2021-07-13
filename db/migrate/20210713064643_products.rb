70d5963dc7deb9ecef069248bb7faba2699a2697
shalisl <42828750+ShaliSL@users.noreply.github.com>
2021-07-13 18:06:24 +0530

    Add files via upload

---
+++ db/migrate/20210713064643_products.rb
@@ -0,0 +1,13 @@
+class Products < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.string :image
+      t.integer :price_cents
+      t.integer :quantity
+
+      t.timestamps null: false
+    end
+  end
+end
