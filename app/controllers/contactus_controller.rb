4b4db9166fdb8c20a4f57fe0abe7da83369ce7c0
shalisl <42828750+ShaliSL@users.noreply.github.com>
2021-07-14 17:43:11 +0530

    Update contactus_controller.rb

--- app/controllers/contactus_controller.rb
+++ app/controllers/contactus_controller.rb
@@ -1,4 +1,11 @@
 class ContactusController < ApplicationController
-  def show
+  def create
+    contacts = Contact.new(contact_params)
+    flash[:success] = "Success"
+    redirect_to '/'
+  end
+
+  def contact_params
+    params.require(:contacts).permit(:name, :address, :message)
   end
 end
