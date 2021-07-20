3e299740668ba72cc5e299db46f8dde3a1175753
shalisl <42828750+ShaliSL@users.noreply.github.com>
2021-07-20 16:15:03 +0530

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,11 @@
 class SessionsController < ApplicationController
-  def index
+  def create
+    users = User.find_by_email(params[:email])
+    puts "User Details #{users}"
+    if users && users.authenticate(params[:password])
+      session[:users_id] = users.id
+      flash[:success] = "Welcome back, #{users.name}!"
+      redirect_to '/'
+    end
   end
 end
