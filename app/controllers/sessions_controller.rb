c3e6e0d1087766445a6ff5244c409b5022c011e5
shalisl <42828750+ShaliSL@users.noreply.github.com>
2021-07-22 15:38:59 +0530

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,18 @@
 class SessionsController < ApplicationController
   def create
     users = User.find_by_email(params[:email])
-    puts "User Details #{users}"
-    if users && users.authenticate(params[:password])
+    puts 'Session list'
+    puts params[:email]
+    if users 
       session[:users_id] = users.id
       flash[:success] = "Welcome back, #{users.name}!"
       redirect_to '/'
+    else
+      flash[:danger] = "We couldn't log you in. Please verify that your email/password is correct."
+      redirect_to '/login'
     end
-  end
+  end 
+ 
 end
+
+
