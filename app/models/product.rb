07c44f3f07b43bc231eab0a18531f2ce8fc13ae5
shalisl <42828750+ShaliSL@users.noreply.github.com>
2021-07-26 18:04:06 +0530

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,5 @@
 
 class Product < ActiveRecord::Base
     validates_presence_of :name, :price, :quantity, :category
-    has_many :reviews, -> { order(created_at: :desc) }
-    belongs_to :category
-
+    mount_uploader :image, ProductImageUploader
 end
