99d6278ab723ece4495b02a289124fc27fad8f80
shalisl <42828750+ShaliSL@users.noreply.github.com>
2021-07-28 17:29:37 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,6 @@ Rails.application.routes.draw do
   get '/contact' => 'contactus#show'
   post '/contactus' => 'contactus#create'
   get '/confirm' => 'orders#confirm'
+  get '/buynow' => 'carts#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
